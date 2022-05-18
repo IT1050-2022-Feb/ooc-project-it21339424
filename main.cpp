49203e9f87e2497fde01faee7d87878e54841add
it21339424 <99307800+it21339424@users.noreply.github.com>
2022-05-18 16:18:39 +0530

    Create main.cpp
    create

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "User.h"
+#include "Guest.h"
+#include "Admin.h"
+#include "Game.h"
+#include "News.h"
+#include "Payment.h"
+#include "Lobby.h"
+#include <cstring>
+using namespace std;
+
+int main() {
+
+  //creating object
+  User U1;
+  User U2( 01, (char *) "Samith", (char *) "samithperera", (char *) "samith@2022" );
+  
+  Admin A1;
+  Admin A2( 03, (char *) "Akidu" );
+  
+  Guest Gu1;
+  Guest Gu2( 04 );
+  
+  Game G1;
+  Game G2( 10, (char *) "COD4" );
+  
+  News N1;
+  News N2( 12, (char *) "Populer games in 2022") ;
+  
+  Payment P1;
+  Payment P2( 01344, (char *) "Visa", 3000.00 );
+  
+  Lobby L1;
+  Lobby L2( 1003 );
+
+  return 0;
+}
